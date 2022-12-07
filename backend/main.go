3cada09ff160dd8fd353ed2aa8d50b1639da5aef
Wisarut Chaloemanukulwattana <wisarutkim012@gmail.com>
2022-12-08 00:15:41 +0700

    add api pet and test

--- backend/main.go
+++ backend/main.go
@@ -1,19 +1,43 @@
 package main
 
 import (
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+
+	"github.com/forceattack012/petAppApi/pet"
 )
 
 func main() {
-	r := gin.Default()
+	if err := godotenv.Load("local.env"); err != nil {
+		log.Fatal("cannot read configuration")
+	}
 
+	dsn := os.Getenv("dsn")
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		panic("connect database failed !!!")
+	}
+
+	db.AutoMigrate(&pet.Pet{})
+	petHandler := pet.NewPetHandler(db)
+
+	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
 		})
 	})
 
-	r.Run(":8081")
+	r.POST("/pet", petHandler.NewPet)
+	r.GET("/pet", petHandler.GetPets)
+
+	port := ":" + os.Getenv("PORT")
+	r.Run(port)
 }
--- backend/pet/pet.go
+++ backend/pet/pet.go
@@ -1,1 +1,69 @@
 package pet
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+)
+
+type Pet struct {
+	gorm.Model
+	Name        string `json:"name"`
+	Type        string `json:"type"`
+	Description string `json:"description"`
+	Age         int64  `json:"age"`
+	// Birthdate   string `json:"birthdate"`
+	// Age         int64  `json:"age"`
+	// Image       []byte `json:"image"`
+	// gorm.Model
+}
+
+func (Pet) Tablename() string {
+	return "pets"
+}
+
+type PetHandler struct {
+	db *gorm.DB
+}
+
+func NewPetHandler(db *gorm.DB) *PetHandler {
+	return &PetHandler{
+		db: db,
+	}
+}
+
+func (h *PetHandler) NewPet(c *gin.Context) {
+	var pet Pet
+	if err := c.ShouldBindJSON(&pet); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	result := h.db.Create(&pet)
+	if err := result.Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": err.Error,
+		})
+		return
+	}
+
+	c.JSON(http.StatusCreated, gin.H{
+		"ID": pet.Model.ID,
+	})
+}
+
+func (h *PetHandler) GetPets(c *gin.Context) {
+	var pets []Pet
+	result := h.db.Find(&pets)
+
+	if err := result.Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": err.Error,
+		})
+		return
+	}
+	c.JSON(http.StatusOK, pets)
+}
